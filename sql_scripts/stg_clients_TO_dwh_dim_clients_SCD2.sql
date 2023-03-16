9bcbb9e3928eb11be78274e1694c4d1ac58457c1
Maxim Butuzov <101450815+mazzy8@users.noreply.github.com>
2023-03-16 16:32:57 +0500

    Update stg_clients_TO_dwh_dim_clients_SCD2.sql

--- sql_scripts/stg_clients_TO_dwh_dim_clients_SCD2.sql
+++ sql_scripts/stg_clients_TO_dwh_dim_clients_SCD2.sql
@@ -9,7 +9,7 @@ select
 	stg.passport_valid_to,
 	stg.phone,
 	stg.create_dt,
-	to_date('9999-12-31','YYYY-MM-DD'),
+	to_date('2999-12-31','YYYY-MM-DD'),
 	'N'
 from de12.buma_stg_clients stg
 left join de12.buma_dwh_dim_clients tgt
@@ -25,7 +25,7 @@ from (
 	from de12.buma_stg_clients stg
 	inner join de12.buma_dwh_dim_clients tgt
 		on stg.client_id = tgt.client_id
-		and tgt.end_dt = to_date('9999-12-31','YYYY-MM-DD')
+		and tgt.end_dt = to_date('2999-12-31','YYYY-MM-DD')
 	where stg.last_name <> tgt.last_name or ( stg.last_name is null and tgt.last_name is not null ) or ( stg.last_name is not null and tgt.last_name is null ) or
 		  stg.patronymic <> tgt.patronymic or ( stg.patronymic is null and tgt.patronymic is not null ) or ( stg.patronymic is not null and tgt.patronymic is null ) or
 		  stg.date_of_birth <> tgt.date_of_birth or ( stg.date_of_birth is null and tgt.date_of_birth is not null ) or ( stg.date_of_birth is not null and tgt.date_of_birth is null ) or
@@ -35,7 +35,7 @@ from (
 		  stg.passport_valid_to <> tgt.passport_valid_to or ( stg.passport_valid_to is null and tgt.passport_valid_to is not null ) or ( stg.passport_valid_to is not null and tgt.passport_valid_to is null )
 ) tmp
 where buma_dwh_dim_clients.client_id = tmp.client_id
-  and buma_dwh_dim_clients.end_dt = to_date('9999-12-31','YYYY-MM-DD');
+  and buma_dwh_dim_clients.end_dt = to_date('2999-12-31','YYYY-MM-DD');
 insert into de12.buma_dwh_dim_clients (client_id, last_name, first_name, patronymic, date_of_birth, passport_num, passport_valid_to, phone, start_dt, end_dt, deleted_flg)
 select
 	stg.client_id,
@@ -47,7 +47,7 @@ select
 	stg.passport_valid_to,
 	stg.phone,
 	stg.update_dt,
-	to_date('9999-12-31','YYYY-MM-DD'),
+	to_date('2999-12-31','YYYY-MM-DD'),
 	'N'
 from de12.buma_stg_clients stg
 inner join de12.buma_dwh_dim_clients tgt
@@ -71,13 +71,13 @@ select
 	tgt.passport_valid_to,
 	tgt.phone,
 	now(),
-	to_date('9999-12-31','YYYY-MM-DD'),
+	to_date('2999-12-31','YYYY-MM-DD'),
 	'Y'
 from de12.buma_dwh_dim_clients tgt
 left join de12.buma_stg_clients_del stg
 	on stg.client_id = tgt.client_id
 where stg.client_id is null
-  and tgt.end_dt = to_date('9999-12-31','YYYY-MM-DD')
+  and tgt.end_dt = to_date('2999-12-31','YYYY-MM-DD')
   and tgt.deleted_flg = 'N';
 update de12.buma_dwh_dim_clients
 set
@@ -86,9 +86,9 @@ where client_id in (
 	select tgt.client_id
 	from de12.buma_dwh_dim_clients tgt
 	left join de12.buma_stg_clients_del stg
-		on stg.client_id = tgt.client_id
+		on tgt.client_id = stg.client_id
 	where stg.client_id is Null
-	  and tgt.end_dt = to_date('9999-12-31','YYYY-MM-DD')
+	  and tgt.end_dt = to_date('2999-12-31','YYYY-MM-DD')
       and tgt.deleted_flg = 'N');
 update de12.buma_meta_stg
 set max_update_dt = coalesce( 
