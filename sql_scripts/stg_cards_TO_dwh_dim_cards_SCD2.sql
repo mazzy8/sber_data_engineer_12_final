6d263d87595da943377483ea3547880f9f77db72
Maxim Butuzov <101450815+mazzy8@users.noreply.github.com>
2023-03-16 16:33:48 +0500

    Update stg_cards_TO_dwh_dim_cards_SCD2.sql

--- sql_scripts/stg_cards_TO_dwh_dim_cards_SCD2.sql
+++ sql_scripts/stg_cards_TO_dwh_dim_cards_SCD2.sql
@@ -3,7 +3,7 @@ select
 	stg.card_num,
 	stg.account,
 	stg.create_dt, 
-	to_date('9999-12-31','YYYY-MM-DD'),
+	to_date('2999-12-31','YYYY-MM-DD'),
 	'N'
 from de12.buma_stg_cards stg
 left join de12.buma_dwh_dim_cards tgt
@@ -19,17 +19,17 @@ from (
 	from de12.buma_stg_cards stg
 	inner join de12.buma_dwh_dim_cards tgt
 		on stg.card_num = tgt.card_num
-		and tgt.end_dt = to_date('9999-12-31','YYYY-MM-DD')
+		and tgt.end_dt = to_date('2999-12-31','YYYY-MM-DD')
 	where stg.account <> tgt.account_num or ( stg.account is null and tgt.account_num is not null ) or ( stg.account is not null and tgt.account_num is null )
 ) tmp
 where buma_dwh_dim_cards.card_num = tmp.card_num
-  and buma_dwh_dim_cards.end_dt = to_date('9999-12-31','YYYY-MM-DD');
+  and buma_dwh_dim_cards.end_dt = to_date('2999-12-31','YYYY-MM-DD');
 insert into de12.buma_dwh_dim_cards (card_num, account_num, start_dt, end_dt, deleted_flg)
 select 
 	stg.card_num, 
 	stg.account,
 	stg.update_dt,
-	to_date('9999-12-31','YYYY-MM-DD'),
+	to_date('2999-12-31','YYYY-MM-DD'),
 	'N'
 from de12.buma_stg_cards stg
 inner join de12.buma_dwh_dim_cards tgt
@@ -41,13 +41,13 @@ select
 	tgt.card_num,
 	tgt.account_num,
 	now(),
-	to_date('9999-12-31','YYYY-MM-DD'),
+	to_date('2999-12-31','YYYY-MM-DD'),
 	'Y'
 from de12.buma_dwh_dim_cards tgt
-left join de12.buma_stg_cards_del stg
+    left join de12.buma_stg_cards_del stg
 	on stg.card_num = tgt.card_num
 where stg.card_num is null
-  and tgt.end_dt = to_date('9999-12-31','YYYY-MM-DD')
+  and tgt.end_dt = to_date('2999-12-31','YYYY-MM-DD')
   and tgt.deleted_flg = 'N';
 update de12.buma_dwh_dim_cards
 set 
@@ -55,10 +55,10 @@ set
 where card_num in (
 	select tgt.card_num
 	from de12.buma_dwh_dim_cards tgt
-	left join de12.buma_stg_cards_del stg
-		on stg.card_num = tgt.card_num
+	    left join de12.buma_stg_cards_del stg
+		on tgt.card_num = stg.card_num
 	where stg.card_num is Null
-	  and tgt.end_dt = to_date('9999-12-31','YYYY-MM-DD')
+	  and tgt.end_dt = to_date('2999-12-31','YYYY-MM-DD')
       and tgt.deleted_flg = 'N');
 update de12.buma_meta_stg
 set max_update_dt = coalesce( 
