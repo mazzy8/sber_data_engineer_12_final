62f06681d3c600650002331a4f1a806eedb21556
Maxim Butuzov <101450815+mazzy8@users.noreply.github.com>
2023-03-16 16:31:30 +0500

    Update stg_accounts_TO_dwh_dim_accounts_SCD2.sql

--- sql_scripts/stg_accounts_TO_dwh_dim_accounts_SCD2.sql
+++ sql_scripts/stg_accounts_TO_dwh_dim_accounts_SCD2.sql
@@ -4,7 +4,7 @@ select
 	stg.valid_to , 
 	stg.client ,
 	stg.create_dt, 
-	to_date('9999-12-31','YYYY-MM-DD'),
+	to_date('2999-12-31','YYYY-MM-DD'),
 	'N'
 from de12.buma_stg_accounts stg
 left join de12.buma_dwh_dim_accounts tgt
@@ -20,19 +20,19 @@ from (
 	from de12.buma_stg_accounts stg
 	inner join de12.buma_dwh_dim_accounts tgt
 		on stg.account = tgt.account_num
-		and tgt.end_dt = to_date('9999-12-31','YYYY-MM-DD')
+		and tgt.end_dt = to_date('2999-12-31','YYYY-MM-DD')
 	where stg.valid_to <> tgt.valid_to or ( stg.valid_to is null and tgt.valid_to is not null ) or ( stg.valid_to is not null and tgt.valid_to is null ) or 
 		  stg.client <> tgt.client or ( stg.client is null and tgt.client is not null ) or ( stg.client is not null and tgt.client is null )
 ) tmp
 where buma_dwh_dim_accounts.account_num = tmp.account
-  and buma_dwh_dim_accounts.end_dt = to_date('9999-12-31','YYYY-MM-DD');
+  and buma_dwh_dim_accounts.end_dt = to_date('2999-12-31','YYYY-MM-DD');
 insert into de12.buma_dwh_dim_accounts (account_num, valid_to, client, start_dt, end_dt, deleted_flg)
 select 
 	stg.account, 
 	stg.valid_to,
 	stg.client,
 	stg.update_dt,
-	to_date('9999-12-31','YYYY-MM-DD'),
+	to_date('2999-12-31','YYYY-MM-DD'),
 	'N'
 from de12.buma_stg_accounts stg
 inner join de12.buma_dwh_dim_accounts tgt
@@ -46,13 +46,13 @@ select
 	tgt.valid_to,
 	tgt.client,
 	now(),
-	to_date('9999-12-31','YYYY-MM-DD'),
+	to_date('2999-12-31','YYYY-MM-DD'),
 	'Y'
 from de12.buma_dwh_dim_accounts tgt
 left join de12.buma_stg_accounts_del stg
-	on stg.account = tgt.account_num
+	on tgt.account_num = stg.account
 where stg.account is null
-  and tgt.end_dt = to_date('9999-12-31','YYYY-MM-DD')
+  and tgt.end_dt = to_date('2999-12-31','YYYY-MM-DD')
   and tgt.deleted_flg = 'N';
 update de12.buma_dwh_dim_accounts
 set 
@@ -63,7 +63,7 @@ where account_num in (
 	left join de12.buma_stg_accounts_del stg
 		on tgt.account_num = stg.account
 	where stg.account is Null
-	  and tgt.end_dt = to_date('9999-12-31','YYYY-MM-DD')
+	  and tgt.end_dt = to_date('2999-12-31','YYYY-MM-DD')
       and tgt.deleted_flg = 'N');
 update de12.buma_meta_stg
 set max_update_dt = coalesce( 
