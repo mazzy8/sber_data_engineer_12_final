ca05710e3501ae3babbe0d428163c52b26f6b2a7
Maxim Butuzov <101450815+mazzy8@users.noreply.github.com>
2023-03-16 16:30:11 +0500

    Update stg_terminals_TO_dwh_dim_terminals_SCD2.sql

--- sql_scripts/stg_terminals_TO_dwh_dim_terminals_SCD2.sql
+++ sql_scripts/stg_terminals_TO_dwh_dim_terminals_SCD2.sql
@@ -5,7 +5,7 @@ select
 	stg.terminal_city,
 	stg.terminal_address,
 	now()::date,
-	to_date('9999-12-31','YYYY-MM-DD'),
+	to_date('2999-12-31','YYYY-MM-DD'),
 	'N'
 from de12.buma_stg_terminals stg
 left join de12.buma_dwh_dim_terminals tgt
@@ -20,13 +20,13 @@ from (
 	from de12.buma_stg_terminals stg
 	inner join de12.buma_dwh_dim_terminals tgt
 		on stg.terminal_id = tgt.terminal_id
-		and tgt.end_dt = to_date('9999-12-31','YYYY-MM-DD')
+		and tgt.end_dt = to_date('2999-12-31','YYYY-MM-DD')
 	where stg.terminal_type <> tgt.terminal_type or ( stg.terminal_type is null and tgt.terminal_type is not null ) or ( stg.terminal_type is not null and tgt.terminal_type is null ) or
 		  stg.terminal_city <> tgt.terminal_city or ( stg.terminal_city is null and tgt.terminal_city is not null ) or ( stg.terminal_city is not null and tgt.terminal_city is null ) or
 		  stg.terminal_address <> tgt.terminal_address or ( stg.terminal_address is null and tgt.terminal_address is not null ) or ( stg.terminal_address is not null and tgt.terminal_address is null )
 ) tmp
 where buma_dwh_dim_terminals.terminal_id = tmp.terminal_id
-  and buma_dwh_dim_terminals.end_dt = to_date('9999-12-31','YYYY-MM-DD');
+  and buma_dwh_dim_terminals.end_dt = to_date('2999-12-31','YYYY-MM-DD');
 insert into de12.buma_dwh_dim_terminals (terminal_id, terminal_type, terminal_city, terminal_address, start_dt, end_dt, deleted_flg)
 select
 	stg.terminal_id,
@@ -34,7 +34,7 @@ select
 	stg.terminal_city,
 	stg.terminal_address,
 	now()::date,
-	to_date('9999-12-31','YYYY-MM-DD'),
+	to_date('2999-12-31','YYYY-MM-DD'),
 	'N'
 from de12.buma_stg_terminals stg
 inner join de12.buma_dwh_dim_terminals tgt
@@ -50,13 +50,13 @@ select
 	tgt.terminal_city,
 	tgt.terminal_address,
 	now()::date,
-	to_date('9999-12-31','YYYY-MM-DD'),
+	to_date('2999-12-31','YYYY-MM-DD'),
 	'Y'
 from de12.buma_dwh_dim_terminals tgt
-left join de12.buma_stg_terminals_del stg
-	on stg.terminal_id = tgt.terminal_id
+left join de12.buma_stg_terminals stg
+	on tgt.terminal_id = stg.terminal_id
 where stg.terminal_id is null
-  and tgt.end_dt = to_date('9999-12-31','YYYY-MM-DD')
+  and tgt.end_dt = to_date('2999-12-31','YYYY-MM-DD')
   and tgt.deleted_flg = 'N';
 update de12.buma_dwh_dim_terminals
 set
@@ -64,8 +64,8 @@ set
 where terminal_id in (
 	select tgt.terminal_id
 	from de12.buma_dwh_dim_terminals tgt
-	left join de12.buma_stg_terminals_del stg
+	left join de12.buma_stg_terminals stg
 		on stg.terminal_id = tgt.terminal_id
 	where stg.terminal_id is Null
-	  and tgt.end_dt = to_date('9999-12-31','YYYY-MM-DD')
+	  and tgt.end_dt = to_date('2999-12-31','YYYY-MM-DD')
       and tgt.deleted_flg = 'N');
